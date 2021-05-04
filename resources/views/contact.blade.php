0830ea7c8cff7a276bfa5f4d2c658ff80768c948
Noémie ROCHARD <nrochard@student.42.fr>
2021-05-04 10:44:36 +0200

    navbar ok

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+
+@section('content')
+
+<div class="relative bg-white overflow-hidden">
+    <div class="max-w-7xl mx-auto">
+        <div class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-3xl lg:w-full lg:pb-28 xl:pb-32">
+
+            @include('partials.navbar')
+
+        </div>
+    </div>
+</div>
+
+
+@endsection
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Assedita</title>
+
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
