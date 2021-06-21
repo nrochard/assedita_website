28eebf6edee16a7cd87e8fb48bb2775c36f8c38e
Noémie ROCHARD <nrochard@student.42.fr>
2021-06-22 00:35:57 +0200

    bug fixed routes

--- resources/views/admin/partials/menu-desktop.blade.php
+++ resources/views/admin/partials/menu-desktop.blade.php
@@ -10,11 +10,11 @@
             <i class="fas fa-tachometer-alt mr-3"></i>
             Tableau de bord
         </a>
-        <a href="{{route('users')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+        <a href="{{route('admin.users')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
             <i class="fas fa-sticky-note mr-3"></i>
             Utilisateurs
         </a>
-        <a href="{{route('posts')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
+        <a href="{{route('admin.posts')}}" class="flex items-center text-white opacity-75 hover:opacity-100 py-4 pl-6 nav-item">
             <i class="fas fa-table mr-3"></i>
             Articles
         </a>
