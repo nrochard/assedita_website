8260fb489529619ddf26c5c8769ce6bf2d5492a6
Noémie ROCHARD <nrochard@student.42.fr>
2021-06-22 17:08:15 +0200

    support admin ok

--- resources/views/admin/support.blade.php
+++ resources/views/admin/support.blade.php
@@ -10,15 +10,30 @@
 @include('admin/partials/menu-mobile')
     
         <div class="w-full h-screen overflow-x-hidden border-t flex flex-col">
-            <main class="w-full flex-grow p-6">
-                <h1 class="text-3xl text-black pb-6">Contacter le support</h1>
+        <main class="w-full flex-grow p-6">
+                <h1 class="text-3xl text-black pb-6">Support</h1>
 
-                <!-- Content goes here! 😁 -->
+                <div class="w-full mt-6" x-data="{ openTab: 1 }">
+                    <div>
+                        <ul class="flex border-b">
+                            <li class="-mb-px mr-1" @click="openTab = 1">
+                                <a :class="openTab === 1 ? 'border-l border-t border-r rounded-t text-black font-semibold' : 'text-black hover:text-gray-500'" class="bg-white inline-block py-2 px-4 font-semibold" href="#">Par mail</a>
+                            </li>
+                            <li class="mr-1" @click="openTab = 2">
+                                <a :class="openTab === 2 ? 'border-l border-t border-r rounded-t text-black font-semibold' : 'text-black hover:text-gray-500'" class="bg-white inline-block py-2 px-4 font-semibold" href="#">Par téléphone</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="bg-white p-6">
+                        <div id="" class="" x-show="openTab === 1">
+                            developpeur@assedita.com
+                        </div>
+                        <div id="" class="" x-show="openTab === 2">
+                            Pour toute urgence : 06 39 92 09 49 
+                        </div>
+                    </div>
+                </div>
             </main>
-    
-            <footer class="w-full bg-white text-right p-4">
-                Built by <a target="_blank" href="https://davidgrzyb.com" class="underline">David Grzyb</a>.
-            </footer>
         </div>
         
     </div>
